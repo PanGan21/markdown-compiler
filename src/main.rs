b609ea13eb7445939004cb3a4600c69a19c90a26
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-10-03 17:25:13 +0200

    print environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
+}
+
+fn parse_markdown_file() {}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {}
+
+fn usage() {}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
